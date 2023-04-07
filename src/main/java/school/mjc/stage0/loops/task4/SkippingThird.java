54caff15186d5e87307c7907b73680db7a92c39c
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-07 12:37:01 +0600

    Update SkippingThird.java

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        if (lastPrinted <= 0) {
+            System.out.println(None);
+        }
+        for (int iter = 0; iter < lastPrinted; iter++) {
+            if (iter % 3 == 0) {
+                continue;
+            }
+            System.out.println(iter);
+        }
     }
 }
